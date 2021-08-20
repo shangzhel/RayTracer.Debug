0de86944e5e62ebcb68ed9c26a3e7cea2931949a
Shang Zhe Lee <shangzhel@student.unimelb.edu.au>
2021-08-20 18:12:23 +1000

    Add code comments.

--- Logger.cs
+++ Logger.cs
@@ -6,22 +6,57 @@ using System.Text;
 
 namespace shangzhel.RayTracer.Debug
 {
+    /// <summary>
+    /// Represents a collection of rays to be dumped.
+    /// </summary>
     class Logger
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Logger"/> class.
+        /// </summary>
         public Logger() : this(0)
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Logger"/> class,
+        /// given an initial capacity for the internal buffer.
+        /// </summary>
+        /// <param name="capacity">
+        /// The number of elements that the collection can initially store.
+        /// </param>
+        /// <remarks>
+        /// Use this overload if you know ahead of time approximately how
+        /// many rays you would record to save some memory allocation overhead.
+        /// </remarks>
         public Logger(int capacity)
         {
             rays = new List<DebugRay>(capacity);
         }
 
+        /// <summary>
+        /// Adds a ray to the collection.
+        /// </summary>
+        /// <param name="id">An identifier for the ray.</param>
+        /// <param name="from">The origin of the ray.</param>
+        /// <param name="to">The end point of the ray.</param>
+        /// <remarks>
+        /// Do not reuse or modify the <paramref name="id"/> array after recording,
+        /// because the function does not maintain a separate copy of the array.
+        /// </remarks>
         public void Log(int[] id, Vector3 from, Vector3 to)
         {
             rays.Add(new DebugRay(id, from, to));
         }
 
+        /// <summary>
+        /// Serializes collected rays into a <see cref="Stream"/>.
+        /// </summary>
+        /// <param name="stream">The stream to write to.</param>
+        /// <remarks>
+        /// The collection is not cleared after this method returns. You may call
+        /// this method multiple times to write to multiple <see cref="Stream"/>s.
+        /// </remarks>
         public void WriteToStream(Stream stream)
         {
             var arr = rays.ToArray();
